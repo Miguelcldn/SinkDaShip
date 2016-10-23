b566f8dde0d7e6c45a710bfdcf79f3d92f3ff0bb
Alan Martini <alanmmartini@gmail.com>
2016-10-23 15:03:52 -0200

    connected functions to the server

--- client/app/src/assets/js/service.js
+++ client/app/src/assets/js/service.js
@@ -1,3 +1,4 @@
+
 /*
  IN are functions you call to the engine,
  receiveAttack(position: string): boolean //Function to call when user is attacked, where position is a table position such as "F3". Returns `true` if attack hit and `false` otherwise.
@@ -8,44 +9,41 @@
  @return: boolean
  */
 window.receiveAttack = function(position){
-  //@TODO remove / return to test only
-  return Math.random() >= 0.5;
 
-  $.ajax({
-    url: "http://rnlabs.com.br:8080/sinkdaship/@TODO endpoint for attack",
-    method: "POST",
-    data: {pos:position},
-    beforeSend: function( xhr ) {
-      //something
-    }
-  }).done(function( data ) {
-    //END TURN !important
-    return data;
-  });
 }
 
 /*
  yourTurn(): void //Function to call when it's the players turn, to enable user interaction
  */
 window.yourTurn = function(){
-  //@TODO remove / return to test only
-  return Math.random() >= 0.5;
+  console.log('your turn');
 
-  var sTimeOut = setTimeout(function () {
-    // Depois, em outra página ou aba, recupera esse item
-    var matchId = window.sessionStorage.getItem('matchId');
+  var matchId = window.sessionStorage.getItem('matchId');
+  var playerNumber = window.sessionStorage.getItem('playerNumber');
 
-    $.ajax({
-      url: "http://rnlabs.com.br:8080/sinkdaship/match/player-turn/" + matchId,
-      method: "GET",
-    }).done(function (data) {
-      if(data.ok){
-        console.log('checl in turn');
+  $.ajax({
+    //url: "http://rnlabs.com.br:8080/sinkdaship/match/player-turn/" + matchId,
+    url: 'https://api.portalqualis.com.br:8443/api/recommendations/20422/51804',
+    method: "GET",
+  }).done(function (data) {
+    console.log(data);
+    if(data.ok){
+      if(data.resultObject.playerTurn == playerNumber){
+        console.log('your turn');
+        engine.yourTurn();
       } else {
-        timeOutId = setTimeout(ajaxFn, 10000);
+        setTimeout(function () {
+          yourTurn();
+        }, 3000);
       }
-    });
-  }, 3000);
+    } else {
+      console.log('no return');
+      setTimeout(function () {
+        yourTurn();
+      }, 3000);
+    }
+  });
+
 }
 
 /*
@@ -54,25 +52,22 @@ window.yourTurn = function(){
 window.endGame = function(didWin){
   //@TODO remove / return to test only
   return Math.random() >= 0.5;
-
-
+  engine.endGame(win);
 }
 
 /*
  OUT are functions I will call and you must write its code
  `attack(position: string): void` //Function that will be called when user attacks a position
+
  */
 window.attack = function(position){
   //@TODO remove / return to test only
-  return Math.random() >= 0.5;
+  var matchId = window.sessionStorage.getItem('matchId');
 
   $.ajax({
-    url: "http://rnlabs.com.br:8080/sinkdaship/match/attack/",
+    url: "http://rnlabs.com.br:8080/sinkdaship/match/attack/"+ matchId,
     method: "POST",
-    data: {},
-    beforeSend: function( xhr ) {
-      //something
-    }
+    data: {}
   }).done(function( data ) {
     return data;
   });
@@ -85,18 +80,30 @@ window.lookForMatch = function(name, boardData) {
   window.sessionStorage.removeItem('matchId');
 
   $.ajax({
-    url: "http://rnlabs.com.br:8080/sinkdaship/player/add-new",
+    url: "http://rnlabs.com.br:8080/sinkdaship/match/add-new",
     method: "POST",
     data: {name: name, board: boardData}
   }).done(function( data ) {
+    /*var data = {
+     "ok": true,
+     "resultObject":    {
+     "playerNumber": 1,
+     "matchIdentificator": "999E76BDE5CB535D39CCB651745F6F429E6EFBB593F6CB977E5ED2472AF63724"
+     }
+     };*/
     if(data.ok){
       console.log('Joined Queue');
 
-      matchId = data.resultObject.hashId;
-      console.log('match id ', matchId);
+      var matchId = data.resultObject.matchIdentificator;
+      console.log('Match ID ', matchId);
+
+      var playerNumber = data.resultObject.playerNumber;
+      console.log('Player Number', matchId);
 
       window.sessionStorage.setItem('matchId', matchId);
+      window.sessionStorage.setItem('playerNumber', playerNumber);
 
+      yourTurn();
 
     } else {
       console.log('Error on joining the Queue');
@@ -104,3 +111,6 @@ window.lookForMatch = function(name, boardData) {
     return data;
   });
 }
+
+
+
